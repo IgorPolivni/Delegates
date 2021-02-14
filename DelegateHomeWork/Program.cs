10c06a552e6d71e65035284653704bf72eb00ed3
Poli_tn62@student.itstep.kz <Poli_tn62@student.itstep.kz>
2021-02-14 19:09:17 +0600

    Добавьте файлы проекта.

---
+++ DelegateHomeWork/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace DelegateHomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleColor standartBackGroundColor = Console.BackgroundColor;
+            ConsoleColor standartTextColor = Console.ForegroundColor;
+
+            
+            Button button = new Button
+            {
+                Border = '+',
+                Height = 3,
+                Text = "Мир",
+                Width = 15,
+                BorderColor = ConsoleColor.Green,
+                InternalColor = ConsoleColor.Red,
+                Action = new Handlers(MyFirstMethod)
+
+        };
+            button.Action += new Handlers(MySecondMethod);
+            button.Print();
+
+            Console.BackgroundColor = standartBackGroundColor;
+            Console.ForegroundColor = standartTextColor;
+        }
+
+        private static void MyFirstMethod(object o) { }
+        private static void MySecondMethod(object o) { }
+    }
+}
